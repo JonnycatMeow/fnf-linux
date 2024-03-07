77c2f4ebbde4f085a22be24202d8773eafdd4d10
Jonathan Gomez <107599365+JonnycatMeow@users.noreply.github.com>
2024-03-06 20:04:58 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+#use the sh command  
+sudo apt-get update && sudo apt-get install build-essential
+sudo apt-get install mesa-common-dev libgl1-mesa-dev libglu1-mesa-dev libgl1-mesa-glx 
+sudo apt-get install libxext-dev 
