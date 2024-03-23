53cef0f5739bcbde2eb03ee41394f9b98ed4579d
Jonathan Gomez <107599365+JonnycatMeow@users.noreply.github.com>
2024-03-22 20:40:07 -0700

    Update and rename install.sh to linux.sh

--- install.sh
+++
@@ -1,4 +0,0 @@
-#use the sh command  
-sudo apt-get update && sudo apt-get install build-essential
-sudo apt-get install mesa-common-dev libgl1-mesa-dev libglu1-mesa-dev libgl1-mesa-glx 
-sudo apt-get install libxext-dev 
---
+++ linux.sh
@@ -0,0 +1,2 @@
+#use the sh command  
+sudo apt-get install libgl1-mesa-dev libglu1-mesa-dev g++ g++-multilib gcc-multilib libasound2-dev libx11-dev libxext-dev libxi-dev libxrandr-dev libxinerama-dev
