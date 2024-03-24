a21ecec18412797323b8421541a4524c64815a7e
Jonathan Gomez <107599365+JonnycatMeow@users.noreply.github.com>
2024-03-24 00:30:27 -0700

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,2 +1,5 @@
 #use the sh command  
+#haxe requirements 
 sudo apt-get install libgl1-mesa-dev libglu1-mesa-dev g++ g++-multilib gcc-multilib libasound2-dev libx11-dev libxext-dev libxi-dev libxrandr-dev libxinerama-dev
+#vlc requirements 
+sudo apt install haxe libvlc-dev libvlccore-dev -y
